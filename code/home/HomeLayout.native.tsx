92edf112a81296820dced3f06ef0e6e8503d3dfa
Jack Herrington <jherr@pobox.com>
2024-10-11 11:40:39 -0700

    first version

---
+++ code/home/HomeLayout.native.tsx
@@ -0,0 +1,35 @@
+import { Tabs } from "one";
+import { HomeIcons } from "./HomeIcons";
+import { useTheme } from "tamagui";
+
+export function HomeLayout() {
+  const theme = useTheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.accentColor.val,
+        tabBarInactiveTintColor: theme.gray9.val,
+      }}
+    >
+      <Tabs.Screen
+        name="(feed)"
+        options={{
+          title: "Feed",
+          tabBarIcon: ({ color }) => <HomeIcons.Home size={20} color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="upload"
+        options={{
+          title: "Upload",
+          tabBarIcon: ({ color }) => (
+            <HomeIcons.Camera size={20} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import type { UserConfig } from "vite";
+import { one } from "one/vite";
+import { tamaguiPlugin } from "@tamagui/vite-plugin";
+
+export default {
+  plugins: [
+    one({
+      web: {
+        deploy: "vercel",
+        defaultRenderMode: "ssg",
+      },
+
+      app: {
+        key: "One",
+      },
+    }),
+
+    tamaguiPlugin({
+      optimize: true,
+      components: [
+        "tamagui",
+        "@tamagui-extras/file",
+        "@tamagui-extras/core",
+        "expo-camera",
+      ],
+      config: "./config/tamagui.config.ts",
+      outputCSS: "./code/styles/tamagui.css",
+    }),
+  ],
+} satisfies UserConfig;
